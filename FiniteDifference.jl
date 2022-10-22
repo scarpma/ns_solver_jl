eb5b6671f1b54f39a9528d02953b2852353a7431
scarpma <scarpma@gmail.com>
2022-10-22 12:07:40 +0300

    minor

--- FiniteDifference.jl
+++ FiniteDifference.jl
@@ -1,26 +1,93 @@
 module FiniteDifference
 
+export Field2D
+
 using BandedMatrices # BandedMatrix
 using BlockBandedMatrices # Fill
 using LazyArrays # Kron
 using LinearAlgebra # I
 
-function laplacian_operator(n, h1, h2)
-    # 1d matrix for second order derivative
-    D² = BandedMatrix(0 => Fill(-2,n), 1 => Fill(1,n-1), -1 => Fill(1,n-1))
-    # 2d matrix for ∂_11
-    D_11 = BandedBlockBandedMatrix(Kron(D²/h1^2., Eye(n)))
-    # for 22
-    D_22 = BandedBlockBandedMatrix(Kron(Eye(n), D²/h2^2.))
-    # laplacian
-    Δ = D_11 + D_22
-    return Δ
+using Plots
+
+## function laplacian_operator(n, h1, h2)
+##     # 1d matrix for second order derivative
+##     D² = BandedMatrix(0 => Fill(-2,n), 1 => Fill(1,n-1), -1 => Fill(1,n-1))
+##     # 2d matrix for ∂_11
+##     D_11 = BandedBlockBandedMatrix(Kron(D²/h1^2., Eye(n)))
+##     # for 22
+##     D_22 = BandedBlockBandedMatrix(Kron(Eye(n), D²/h2^2.))
+##     # laplacian
+##     Δ = D_11 + D_22
+##     return Δ
+## end
+## 
+## function CNO(n, dt, Re, h1, h2)
+##     # Crank-Nicholson Operator in Matrix form
+##     Δ = @inline laplacian_operator(n, h1, h2)
+##     return I - (dt/(2*Re))*Δ
+## end
+
+struct Field2D
+    arr
+    h1::AbstractFloat
+    h2::AbstractFloat
+    n1::Int64
+    n2::Int64
+    function Field2D(arr, (h1, h2))
+        @assert ndims(arr) == 2
+        n1 = size(arr,1)
+        n2 = size(arr,2)
+        new(arr, h1, h2, n1, n2)
+    end
+    function Field2D(T, (h1, h2), (n1,n2))
+        arr = Array{T}(undef, (n1,n2))
+        new(arr, h1, h2, n1, n2)
+    end
+    function Field2D(T, (h1, h2), (n1,n2), val)
+        @assert typeof(val) == T
+        arr = Array{T}(undef, (n1,n2))
+        fill!(arr, val)
+        new(arr, h1, h2, n1, n2)
+    end
+    Base.iterate(foo::Field2D, state = 1) = state > fieldcount(Field2D) ? nothing : (getfield(foo, state), state + 1)
 end
 
-function CNO(n, dt, Re, h1, h2)
-    # Crank-Nicholson Operator in Matrix form
-    Δ = @inline laplacian_operator(n, h1, h2)
-    return I - (dt/(2*Re))*Δ
+function dd1!(u::Field2D, o::Field2D)
+
+    arr, h1, h2, n1, n2 = u
+    res, h1p, h2p, n1p, n2p = o
+    @assert n1 == n1p
+    @assert n2 == n2p
+    @assert h1 == h1p
+    @assert h2 == h2p
+
+    #res = Array{eltype(arr)}(undef, (n1,n2))
+    
+    for j=1:n2
+        res[1,j] = (arr[1,j] -2*arr[2,j] + arr[3,j]) / h1^2
+        res[n1,j] = (arr[n1-2,j] -2*arr[n1-1,j] + arr[n1,j]) / h1^2
+    end
+    for i=2:n1-1
+        for j=1:n2
+            res[i,j] = (arr[i-1,j] -2*arr[i,j] + arr[i+1,j]) / h1^2
+        end
+    end
+    return res
+end
+
+function test_dd1()
+    f = Field2D(Float64, (0.1,0.1), (100,100))
+    u, h1, h2, n1, n2 = f
+    for j=1:n2
+        for i=1:n1
+            u[i,j] = cos(i*h1 + j*h2)
+        end
+    end
+    p1 = heatmap(u, title="u")
+    dd1u = dd1(f)
+    p2 = heatmap(dd1u, title="d11 u")
+    plot(p1, p2)
+    gui()
 end
 
 function LaplacianTriDiagSolve!(v, d, h) # mutated argument in first position
@@ -50,11 +117,36 @@ function LaplacianTriDiagSolve!(v, d, h) # mutated argument in first position
     return
 end
 
-function test_LaplacianTriDiagSolve()
+
+function test_LaplacianTriDiagSolve1()
     n = 10000
     RHS = rand(Float64, (n))
     u = zeros(Float64, (n))
     FiniteDifference.LaplacianTriDiagSolve!(u, RHS, 1.0)
+    @show u
+    
+    residual = 0.
+    tmp = 0.
+
+    tmp = (-2*u[1] + u[2])/h1 - RHS[1]
+    println("Res[", 0, "]: ", tmp)
+    residual += tmp
+    for i=2:n-1
+        tmp = (u[i-1]-2*u[i] + u[i+1])/h1 - RHS[i]
+        println("Res[", i, "]: ", tmp)
+        residual += tmp
+    end
+    tmp = (u[n-1] -2*u[n])/h1 - RHS[n]
+    println("Res[", n, "]: ", tmp)
+    residual += tmp
+    @show residual
+end
+function test_LaplacianTriDiagSolve2()
+    n = 1000
+    f = Field2D(Float64, (1.0,1.0), (n,n), 0.0)
+    RHS = rand(Float64, (n))
+    u, h1 = f
+    FiniteDifference.LaplacianTriDiagSolve!(@view(u[:,1]), RHS, 1.0)
     @show u
     
     residual = 0.
--- main.jl
+++ main.jl
@@ -3,6 +3,7 @@ include("./FiniteDifference.jl")
 using .IO
 using .FiniteDifference
 
+const T = Float64
 const Re = 100.
 const dt = 1.0
 const n1 = 50
@@ -19,12 +20,14 @@ y = collect(range(start=0., step=h1, length=n1))
 xp = collect(range(start=-h2/2., step=h2, length=n2+1))
 yp = collect(range(start=(n1-1)*h1+h1/2, step=-h1, length=n1+1))
 
-u = zeros(Float32, (n1,n2))
-v = zeros(Float32, (n1,n2))
-p = zeros(Float32, (n1+1,n2+1))
+u = Field2D(T, (h1,h2), (n1,n2), 0.)
+v = Field2D(T, (h1,h2), (n1,n2), 0.)
+p = Field2D(T, (h1,h2), (n1+1,n2+1), 0.)
 
-IO.writeFieldsToFile("prova",x,y,u,v,p)
-IO.writeSingleFieldToFile("prova_p",xp,yp,p,"p")
+FiniteDifference.test_dd1()
+
+#IO.writeFieldsToFile("prova",x,y,u,v,p)
+#IO.writeSingleFieldToFile("prova_p",xp,yp,p,"p")
 
 end
 
