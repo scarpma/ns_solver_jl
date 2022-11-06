bbadc86c7b917a2d01ea95a4afbd4727d965b1f9
scarpma <scarpma@gmail.com>
2022-11-06 15:08:26 +0200

    changed bending forces to the one from fortran code

--- springs.jl
+++ springs.jl
@@ -2,7 +2,7 @@ include("./IO.jl")
 include("./myMesh.jl")
 using .IO
 using .myMesh
-using LinearAlgebra
+#using LinearAlgebra: cross
 using Printf
 
 function expand_mesh(m, pnormals, A)
@@ -13,13 +13,17 @@ function expand_mesh(m, pnormals, A)
     return m2
 end
 
-function compute_strains!(strains, m)
-    d = zeros(eltype(strains), m.ne)
-    compute_distances!(d, m)
+function compute_strains!(strains, d, m)
     d0 = m.lenOfEdges0
     for i=1:m.ne
         strains[i] = (d[i] - d0[i]) / d[i]
     end
+end
+
+function cross!(res, a, b)
+    res[1] = a[2] * b[3] - a[3] * b[2]
+    res[2] = a[3] * b[1] - a[1] * b[3]
+    res[3] = a[1] * b[2] - a[2] * b[1]
 end
 
 function internal_forces!(forces, m, E2d, kb, ke, d, areas, thetas, normals, potenergy, tmp)
@@ -31,20 +35,25 @@ function internal_forces!(forces, m, E2d, kb, ke, d, areas, thetas, normals, pot
     b12::T = 1.
     b22::T = 0.
     beta::T = 0.
-    a11 = zeros(T, 3)
-    a12 = zeros(T, 3)
+    modcsi::T = 0.
+    modzet::T = 0.
     a21 = zeros(T, 3)
-    a22 = zeros(T, 3)
-    a23 = zeros(T, 3)
     a31 = zeros(T, 3)
     a32 = zeros(T, 3)
-    a33 = zeros(T, 3)
-    a41 = zeros(T, 3)
+    a13 = zeros(T, 3)
+    a34 = zeros(T, 3)
     a42 = zeros(T, 3)
+    a23 = zeros(T, 3)
+    a24 = zeros(T, 3)
+    csi = zeros(T, 3)
+    zet = zeros(T, 3)
+    tvec1 = zeros(T, 3)
+    tvec2 = zeros(T, 3)
+    tvec3 = zeros(T, 3)
+    tvec4 = zeros(T, 3)
+
 
     # in plane springs
-    compute_triaNormalAndArea!(normals, areas, m)
-    compute_distances!(d, m)
     d0 = m.lenOfEdges0
     # update spring constants
     for i=1:m.ne
@@ -66,12 +75,13 @@ function internal_forces!(forces, m, E2d, kb, ke, d, areas, thetas, normals, pot
 
     # bending springs
     thetas0 = m.angleOfEdges0
+    #thetas0 = zeros(Float64, m.ne)
     compute_angle!(thetas, m, normals)
     for i=1:m.ne
         t1, t2 = m.triaOfEdge[:,i]
         # if edge is not on boundary
         if (t1 != 0 && t2 != 0)
-            v1, v2, v3, v4 = m.vert1234OfEdge
+            v1, v2, v3, v4 = m.vert1234OfEdge[:,i]
             x1 = m.xyzOfVert[:,v1]
             x2 = m.xyzOfVert[:,v2]
             x3 = m.xyzOfVert[:,v3]
@@ -83,22 +93,67 @@ function internal_forces!(forces, m, E2d, kb, ke, d, areas, thetas, normals, pot
             b11 = - cos(thetas[i])
             b22 = b11
             beta = kb * (sin(thetas[i])*cos(thetas0[i]) - cos(thetas[i])*sin(thetas0[i])) / sqrt(1. - cos(thetas[i])^2.)
-            a11 = LinearAlgebra.cross(n1, x3 - x2)
-            a12 = LinearAlgebra.cross(n2, x3 - x2)
-            a21 = LinearAlgebra.cross(n1, x1 - x3)
-            a22 = LinearAlgebra.cross(n1, x3 - x4) + LinearAlgebra.cross(n2, x1 - x3)
-            a23 = LinearAlgebra.cross(n2, x3 - x4)
-            a31 = LinearAlgebra.cross(n1, x2 - x1)
-            a32 = LinearAlgebra.cross(n1, x3 - x2) + LinearAlgebra.cross(n2, x2 - x1)
-            a33 = LinearAlgebra.cross(n2, x4 - x2)
-            a41 = LinearAlgebra.cross(n1, x2 - x3)
-            a42 = LinearAlgebra.cross(n2, x2 - x3)
+            ## a11 = cross(n1, x3 - x2)
+            ## a12 = cross(n2, x3 - x2)
+            ## a21 = cross(n1, x1 - x3)
+            ## a22 = cross(n1, x3 - x4) + cross(n2, x1 - x3)
+            ## a23 = cross(n2, x3 - x4)
+            ## a31 = cross(n1, x2 - x1)
+            ## a32 = cross(n1, x4 - x2) + cross(n2, x2 - x1)
+            ## a33 = cross(n2, x4 - x2)
+            ## a41 = cross(n1, x2 - x3)
+            ## a42 = cross(n2, x2 - x3)
+
+            ## # compute bending spring forces on v1 v2 v3 v4
+            ## forces[:, v1] = forces[:, v1] + beta .* (b11.*a11 + b12.*a12)
+            ## forces[:, v2] = forces[:, v2] + beta .* (b11.*a21 + b12.*a22 + b22.*a23)
+            ## forces[:, v3] = forces[:, v3] + beta .* (b11.*a31 + b12.*a32 + b22.*a33)
+            ## forces[:, v4] = forces[:, v4] + beta .* (b11.*a41 + b22.*a42)
 
             # compute bending spring forces on v1 v2 v3 v4
-            forces[:, v1] += beta * (b11*a11 + b12*a12)
-            forces[:, v2] += beta * (b11*a21 + b12*a22 + b22*a23)
-            forces[:, v3] += beta * (b11*a31 + b12*a32 + b22*a33)
-            forces[:, v4] += beta * (b11*a41 + b22*a42)
+            
+            a21[:] = x2 - x1
+            a31[:] = x3 - x1
+            a32[:] = x3 - x2
+            a13[:] = x1 - x3
+            a34[:] = x3 - x4
+            a42[:] = x4 - x2
+            a23[:] = x2 - x3
+            a24[:] = x2 - x4
+
+            cross!(csi, a21,a31)
+            cross!(zet, a34,a24)
+            modcsi = sqrt(sum(csi.^2.))
+            modzet = sqrt(sum(zet.^2.))
+
+            b11 = -beta * cos(thetas[i])/(modcsi^2.)
+            b12 = +beta / (modcsi * modzet)
+            b22 = -beta * cos(thetas[i])/(modzet^2.)
+
+            # v1
+            cross!(tvec1, csi, a32)
+            cross!(tvec2, zet, a32)
+            forces[:,v1] = forces[:,v1] + b11.*tvec1 + b12.*tvec2
+
+            # v2
+            cross!(tvec1, csi, a13)
+            cross!(tvec2, csi, a34)
+            cross!(tvec3, zet, a13)
+            cross!(tvec4, zet, a34)
+            forces[:,v2] = forces[:,v2] + b11.*tvec1 + b12.*(tvec2+tvec3) + b22.*tvec4
+
+            # v3
+            cross!(tvec1, csi, a21)
+            cross!(tvec2, csi, a42)
+            cross!(tvec3, zet, a21)
+            cross!(tvec4, zet, a42)
+            forces[:,v3] = forces[:,v3] + b11.*tvec1 + b12.*(tvec2+tvec3) + b22.*tvec4
+
+            # v4
+            cross!(tvec1, csi, a23)
+            cross!(tvec2, zet, a23)
+            forces[:,v4] = forces[:,v4] + b12.*tvec1 + b22.*tvec2
+
             potenergy[1] += kb * (1 - cos(thetas[i] - thetas0[i]))
         end
     end
@@ -123,14 +178,15 @@ end
 function main()
 T = Float64
 dt::T = 0.0001      # time step [s]
-tprint = 0.0001       # save file time step [s]
+tprint = 0.01       # save file time step [s]
 density = 1060*1.5  # mass density of surface [kg / m^3]
 E = 1.e5            # young modulus [Pa]
-B = 1e-3             # bending modulus
+#E = 0.              # young modulus [Pa]
+B = 1.              # bending modulus
 thickness = 0.002   # thickness of the surface [m]
 E2d = E*thickness
 kb = B * 2. / sqrt(3.)
-nsteps = 100
+nsteps = 10000
 
 coords, vertOfTria, vertOfEdge,
 vertOfVert, edgeOfTria, nEdgeVert, 
@@ -165,14 +221,15 @@ for i=0:nsteps
     
     compute_triaNormalAndArea!(normals, areas, m2)
     compute_distances!(d, m2)
-    #compute_angle!(thetas, m, normals)
+    compute_triaNormalAndArea!(normals, areas, m2)
+    compute_angle!(thetas, m2, normals)
 
     rms_v = sqrt.(sum(abs2.(v), dims=2) ./ size(v,2)) 
     a_mean = sum(a, dims=2) / size(a, 2)
     internal_forces!(forces, m2, E2d, kb, ke, d, areas, thetas, normals, potenergy, tmp)
     tot_force = sum(forces, dims=2)
     compute_kenergy!(kenergy, v, mass)
-    compute_strains!(strains, m2)
+    compute_strains!(strains, d, m2)
     a = forces ./ mass
 
     # write files if necessary
@@ -180,10 +237,10 @@ for i=0:nsteps
         myMesh.edgeArrayToTriaArray!(tstrains, m2, strains)
         myMesh.edgeArrayToTriaArray!(dtheta, m2, thetas - m.angleOfEdges0)
         IO.writeSurface(
-            @sprintf("prova_expanded_%5.5d",i),
+            @sprintf("prova_expanded_%5.5d",div(i,Int64(tprint/dt))),
             m2.xyzOfVert, m2.vertOfTria,
             ["forces", "strains", "dtheta"],
-            forces, tstrains, dtheta)
+            forces, tstrains, dtheta./(360/2pi))
     end
 
     # compute stats
@@ -202,7 +259,6 @@ for i=0:nsteps
 
     # integration
     velocity_verlet!(m2.xyzOfVert,v,a,am,dt)
-    println("outside verlet ", m2.xyzOfVert[:,1])
     am = a
 end
 end
